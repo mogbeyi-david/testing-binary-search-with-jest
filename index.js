1e1b91c4e1c0dec3bf6f29131b69d01b05767f53
David Mogbeyi <mogbeyidavid@gmail.com>
2018-07-23 18:19:00 +0100

    Little code and text formatting

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const binarySearch = (array, number) => {
   "use strict";
   const sortedArray = array.sort((a, b) => (a - b));
   const arrayLength = sortedArray.length;
-  const mid = Math.floor(arrayLength / 2);
-  if (number !== array[0] && number !== array[arrayLength - 1] && number !== array[mid]) {
+  const middle = Math.floor(arrayLength / 2);
+  if (number !== array[0] && number !== array[arrayLength - 1] && number !== array[middle]) {
     if (arrayLength <= 2) {
       return false;
     }
 
-    if (number > array[mid]) {
+    if (number > array[middle]) {
       const newArray = [];
-      for (let i = array[mid]; i < arrayLength; i++) {
+      for (let i = array[middle]; i < arrayLength; i++) {
         newArray.push(array[i]);
       }
       binarySearch(newArray, number);
-    } else if (number < array[mid]) {
+    } else if (number < array[middle]) {
       const newArray = [];
-      for (let i = 0; i < array[mid]; i++) {
+      for (let i = 0; i < array[middle]; i++) {
         newArray.push(array[i]);
       }
       binarySearch(newArray, number);
--- index.test.js
+++ index.test.js
@@ -1,41 +1,41 @@
 const binarySearch = require('./index');
 
-test('Testing when the number is in the array', () => {
+test('Testing the binary search function when the number is in the array', () => {
   "use strict";
   expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 5)).toBeTruthy();
 });
 
-test('Testing the Binary Search when the number is not in the array', () => {
+test('Testing the Binary Search function when the number is not in the array', () => {
   "use strict";
   expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 15)).toBeFalsy();
 });
 
-test('Testing the Binary Search when only one parameter is passed', () => {
+test('Testing the Binary Search function when only one parameter is passed', () => {
   "use strict";
   expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9])).toBeFalsy();
 });
 
-test('Testing the Binary Search when a String is passed as a second parameter', () => {
+test('Testing the Binary Search function when a String is passed as a second parameter', () => {
   "use strict";
   expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], '5')).toBeFalsy();
 });
 
-test('Testing the Binary Search when the number is the first digit', () => {
+test('Testing the Binary Search function when the number is the first digit', () => {
   "use strict";
   expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 1)).toBeTruthy();
 });
 
-test('Testing the Binary Search when the number is the last digit', () => {
+test('Testing the Binary Search function when the number is the last digit', () => {
   "use strict";
   expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 9)).toBeTruthy();
 });
 
-test('Testing the Binary Search when the number is immediately the middle digit', () => {
+test('Testing the Binary Search function when the number is immediately the middle digit', () => {
   "use strict";
   expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 5)).toBeTruthy();
 });
 
-test('Testing the Binary Search when the length of the array is zero', () => {
+test('Testing the Binary Search function when the length of the array is zero', () => {
   "use strict";
   expect(binarySearch([], 5)).toBeFalsy();
 });
